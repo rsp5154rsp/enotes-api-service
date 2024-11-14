e78cf4b861128075e7b1d498bc894b48e661d57d
rsp5154rsp <rajeshparanjape2105@gmail.com>
2024-11-14 11:56:40 +0530

    save get category implement

---
+++ src/main/java/com/enotesapp/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.enotesapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enotesapp.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+	
+	
+}
